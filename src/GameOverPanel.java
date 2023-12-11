9984b88e764d51947f583854a4dd8816a44b23d9
YeWon Seo <syw1236@naver.com>
2023-12-12 03:42:41 +0900

    Update GameOverPanel.java
    게임 끝날 시에 클라이언트 정보 초기화 시켜서 전달함

--- src/GameOverPanel.java
+++ src/GameOverPanel.java
@@ -12,11 +12,14 @@ public class GameOverPanel extends JPanel {
     Vector<ClientInfo> clientInfos;
     AppleGameClient appleGameClient;
     ReadyPanel readyPanel;
-    public GameOverPanel(AppleGameClient appleGameClient,Vector<ClientInfo> clientInfos){
+    JDialog dialog;
+    private ActionListener homeButtonListener;
+    public GameOverPanel(AppleGameClient appleGameClient,Vector<ClientInfo> clientInfos,JDialog dialog){
         setLayout(null);
 
+        this.dialog = dialog;
         this.clientInfos = clientInfos;
-        Collections.sort(clientInfos); //클라이언트 정보를 점수 기준으로 정렬함
+        Collections.sort(clientInfos, Collections.reverseOrder()); //클라이언트 정보를 점수 기준으로 정렬함
 
         JLabel rankLabel = new JLabel("순위");
         rankLabel.setOpaque(true);
@@ -41,19 +44,19 @@ public class GameOverPanel extends JPanel {
                 //클라이언트의 정보를 초기화함
                 //대기화면으로 돌아감
                 //클라이언트들의 점수 & ready 여부를 초기화 시켜야함
-                resetClientInfo(); //클라이언트 정보를 초기화시킴
+                Vector<ClientInfo> clientInfoVector = resetClientInfo(); //클라이언트 정보를 초기화시킴
 
-                Window window = SwingUtilities.getWindowAncestor(GameOverPanel.this);
-                if (window instanceof JDialog) {
-                    ((JDialog) window).dispose(); //다이얼로그를 없앰
-                }
-
+                dialog.setVisible(false);
+//
                 try {
-                    appleGameClient.makeReadyScreen(clientInfos); //대기 화면으로 이동
+                    appleGameClient.goToReady(clientInfoVector); //다이얼로그 창 지우고 ready화면으로 이동
+
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
-                appleGameClient.repaint();
+//                AppleGameClientMain appleGameClientMain = new AppleGameClientMain();
+
+//                appleGameClient.setGoToLogin(true);
             }
         });
         add(homeBtn);
@@ -63,15 +66,18 @@ public class GameOverPanel extends JPanel {
         homeDes.setBounds(380,440,150,30);
         add(homeDes);
 
+        requestFocus();
+
 
 
       //  setSize(50)
     }
-    public void resetClientInfo(){ //클라이언트 정보를 초기화시킴
+    public Vector<ClientInfo> resetClientInfo(){ //클라이언트 정보를 초기화시킴
         for(ClientInfo clientInfo : clientInfos){
             clientInfo.setScore(0);
             clientInfo.setIsReady(false);
         }
+        return  clientInfos;
     }
     public Vector<ClientInfo> getResetClientInfos(){
         return this.clientInfos;
@@ -89,20 +95,20 @@ public class GameOverPanel extends JPanel {
             String rankSt = i+1+"위";
             rankNumLabel.setText(rankSt);
             rankNumLabel.setFont(new Font("Arial", Font.BOLD, 30));
-            rankNumLabel.setBounds(160,i*10+98,50,30);
+            rankNumLabel.setBounds(160,i*50+98,50,30);
             add(rankNumLabel);
 
             String name = clientInfo.getName();
             JLabel nameLabel = new JLabel();
             nameLabel.setText(name);
             nameLabel.setFont(new Font("Arial", Font.BOLD, 25));
-            nameLabel.setBounds(230,i*10+100,100,30);
+            nameLabel.setBounds(230,i*50+100,100,30);
             add(nameLabel);
 
             int score = clientInfo.getScore();
             JLabel scoreLabel = new JLabel(String.valueOf(score));
             scoreLabel.setFont(new Font("Arial", Font.BOLD, 25));
-            scoreLabel.setBounds(330,i*10+100,100,30);
+            scoreLabel.setBounds(330,i*50+100,100,30);
             add(scoreLabel);
 
         }
